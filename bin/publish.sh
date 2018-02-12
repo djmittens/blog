ce99b90135d1387a78bfd6cf06598fd9714ed4fe
Nick Gridinskiy <nikita.gridinskiy@gmail.com>
2018-02-12 00:07:50 -0800

    dockerizing the build.

--- bin/publish.sh
+++ bin/publish.sh
@@ -2,18 +2,24 @@
 
 if [[ $(git status -s) ]]
 then
-echo "Working directory is dirty, please commit."
-exit 1;
+    echo "Working directory is dirty, please commit."
+    exit 1;
 fi
 
 echo "Fetching gh-pages into ./public/"
 git worktree add -B gh-pages public origin/gh-pages
 
 echo "Clearing old files"
+mkdir public
 rm -rf public/*
 
 echo "Generating site"
-hugo --theme=hugo_theme_robust --buildDrafts
+#hugo --theme=hugo_theme_robust --buildDrafts
+docker run --rm --name "hugo" -P \
+       -e HUGO_THEME="hugo_theme_robust" \
+       -v $(pwd):/src  \
+       -v public:/output \
+       jojomi/hugo
 
 echo "Publishing..."
 cd public && git commit -am "Publishing new version of the blog" && git push && cd ..
