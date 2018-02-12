5dde2893c80031f088ff4ab20374d78c1d4efb2b
Nick Gridinskiy <nikita.gridinskiy@gmail.com>
2018-02-12 01:16:36 -0800

    fixing the permissions issue in a different way

--- bin/build.sh
+++ bin/build.sh
@@ -3,6 +3,7 @@
 echo "Generating site"
 #hugo --theme=hugo_theme_robust --buildDrafts
 docker run --rm --name "hugo" -P \
+       -u $(id -u):$(id -g)\
        -e HUGO_THEME="hugo_theme_robust" \
        -e HUGO_DESTINATION="/output" \
        -v $(pwd):/src  \
