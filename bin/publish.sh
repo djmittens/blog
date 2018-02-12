1296552f6fe1deb714554bb88dd5d602a461d352
Nick Gridinskiy <nikita.gridinskiy@gmail.com>
2018-02-11 23:49:30 -0800

    publishing is more serious now

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,3 +1,25 @@
-#!/bin/sh
+#!/bin/bash
+
+if [[ $(git status -s) ]]
+then
+echo "Working directory is dirty, please commit."
+exit 1;
+fi
+
+echo "Fetching gh-pages into ./public/"
+git worktree add -B gh-pages public origin/gh-pages
+
+echo "Clearing old files"
+rm -rf public/*
+
+echo "Generating site"
 hugo --theme=hugo_theme_robust --buildDrafts
+
+echo "Publishing..."
 cd public && git commit -am "Publishing new version of the blog" && git push && cd ..
+
+echo "Deleting publication"
+rm -rf public
+mkdir public
+git worktree prune
+rm -rf .git/worktree/public
