a170dbbe38591b2dd80c3d425b1fa2ef90863bdb
Nick Gridinskiy <nick.gridinskiy@creditkarma.com>
2018-02-18 11:20:34 -0800

    Messing around with serve settings.

--- bin/serve.sh
+++ bin/serve.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-docker run --rm --name "hugo" -P \
+docker run --rm -ti --name "hugo" \
        -u $(id -u):$(id -g)\
        -e HUGO_THEME="hugo_theme_robust" \
        -e HUGO_WATCH="true" \
+       -e HUGO_BASEURL="localhost:1313/" \
        -e HUGO_REFRESH_TIME="10" \
+       -p 1313:1313 \
        -v /$(pwd)/:/src  \
        -v /$(pwd)/public:/output \
        jojomi/hugo
